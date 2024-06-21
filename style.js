f2297002995c18ac032832f0a8e621608dd81d96
sharu-35 <sharuit123@gmail.com>
2024-06-21 12:13:13 +0530

    remove action added

--- style.js
+++ style.js
@@ -8,6 +8,11 @@ function textinfo(){
   const textinput = input.value;
   const create = document.createElement('li');
   create.textContent=textinput;
+  create.addEventListener('dblclick',deleteTask);
   list1.appendChild(create);
   textinput.value='';
+
+}
+function deleteTask(event){
+  event.target.remove();
 }
